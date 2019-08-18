5256dbd80aa25b7e056213670faffed01857e270
vocx-fc <ecabreracs@gmail.com>
2019-08-17 23:08:17 -0500

    Draft: removed GuiCommands AddToGroup(), SelectGroup() from DraftTools.py

--- src/Mod/Draft/DraftTools.py
+++ src/Mod/Draft/DraftTools.py
@@ -542,49 +542,6 @@ class SubelementModify(Modifier):
                 # This can occur if objects have had graph changing operations
                 pass
 
-class AddToGroup():
-    """The AddToGroup FreeCAD command definition"""
-
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_AddToGroup',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_AddToGroup", "Move to group..."),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_AddToGroup", "Moves the selected object(s) to an existing group")}
-
-    def IsActive(self):
-        if FreeCADGui.Selection.getSelection():
-            return True
-        else:
-            return False
-
-    def Activated(self):
-        self.groups = ["Ungroup"]
-        self.groups.extend(Draft.getGroupNames())
-        self.labels = ["Ungroup"]
-        for g in self.groups:
-            o = FreeCAD.ActiveDocument.getObject(g)
-            if o: self.labels.append(o.Label)
-        self.ui = FreeCADGui.draftToolBar
-        self.ui.sourceCmd = self
-        self.ui.popupMenu(self.labels)
-
-    def proceed(self,labelname):
-        self.ui.sourceCmd = None
-        if labelname == "Ungroup":
-            for obj in FreeCADGui.Selection.getSelection():
-                try:
-                    Draft.ungroup(obj)
-                except:
-                    pass
-        else:
-            if labelname in self.labels:
-                i = self.labels.index(labelname)
-                g = FreeCAD.ActiveDocument.getObject(self.groups[i])
-                for obj in FreeCADGui.Selection.getSelection():
-                    try:
-                        g.addObject(obj)
-                    except:
-                        pass
-
 
 class WireToBSpline(Modifier):
     """The Draft_Wire2BSpline FreeCAD command definition"""
@@ -630,38 +587,6 @@ class WireToBSpline(Modifier):
                                 self.doc.recompute()
                         else:
                             self.finish()
-
-
-class SelectGroup():
-    """The SelectGroup FreeCAD command definition"""
-
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_SelectGroup',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_SelectGroup", "Select group"),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_SelectGroup", "Selects all objects with the same parents as this group")}
-
-    def IsActive(self):
-        if FreeCADGui.Selection.getSelection():
-            return True
-        else:
-            return False
-
-    def Activated(self):
-        sellist = []
-        sel = FreeCADGui.Selection.getSelection()
-        if len(sel) == 1:
-            if sel[0].isDerivedFrom("App::DocumentObjectGroup"):
-                cts = Draft.getGroupContents(FreeCADGui.Selection.getSelection())
-                for o in cts:
-                    FreeCADGui.Selection.addSelection(o)
-                return
-        for ob in sel:
-            for child in ob.OutList:
-                FreeCADGui.Selection.addSelection(child)
-            for parent in ob.InList:
-                FreeCADGui.Selection.addSelection(parent)
-                for child in parent.OutList:
-                    FreeCADGui.Selection.addSelection(child)
 
 
 class SetAutoGroup():
@@ -819,8 +744,7 @@ FreeCADGui.addCommand('Draft_ToggleConstructionMode',ToggleConstructionMode())
 FreeCADGui.addCommand('Draft_ToggleContinueMode',ToggleContinueMode())
 FreeCADGui.addCommand('Draft_ApplyStyle',ApplyStyle())
 FreeCADGui.addCommand('Draft_ToggleDisplayMode',ToggleDisplayMode())
-FreeCADGui.addCommand('Draft_AddToGroup',AddToGroup())
-FreeCADGui.addCommand('Draft_SelectGroup',SelectGroup())
+
 
 
 
