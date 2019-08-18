68c39c33f227aac4f3e24cc0d04faba97015fa65
vocx-fc <ecabreracs@gmail.com>
2019-08-17 22:33:21 -0500

    Draft: removed GuiCommands FinishLine(), CloseLine(), UndoLine() from DraftTools.py

--- src/Mod/Draft/DraftTools.py
+++ src/Mod/Draft/DraftTools.py
@@ -354,64 +354,6 @@ class Creator(DraftTool):
             self.support = getSupport()
 
 
-class FinishLine:
-    """a FreeCAD command to finish any running Line drawing operation"""
-
-    def Activated(self):
-        if (FreeCAD.activeDraftCommand != None):
-            if (FreeCAD.activeDraftCommand.featureName == "Line"):
-                FreeCAD.activeDraftCommand.finish(False)
-
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_Finish',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_FinishLine", "Finish line"),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_FinishLine", "Finishes a line without closing it")}
-
-    def IsActive(self):
-        if FreeCADGui.ActiveDocument:
-            return True
-        else:
-            return False
-
-
-class CloseLine:
-    """a FreeCAD command to close any running Line drawing operation"""
-
-    def Activated(self):
-        if (FreeCAD.activeDraftCommand != None):
-            if (FreeCAD.activeDraftCommand.featureName == "Line"):
-                FreeCAD.activeDraftCommand.finish(True)
-
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_Lock',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_CloseLine", "Close Line"),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_CloseLine", "Closes the line being drawn")}
-
-    def IsActive(self):
-        if FreeCADGui.ActiveDocument:
-            return True
-        else:
-            return False
-
-
-class UndoLine:
-    """a FreeCAD command to undo last drawn segment of a line"""
-
-    def Activated(self):
-        if (FreeCAD.activeDraftCommand != None):
-            if (FreeCAD.activeDraftCommand.featureName == "Line"):
-                FreeCAD.activeDraftCommand.undolast()
-
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_Rotate',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_UndoLine", "Undo last segment"),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_UndoLine", "Undoes the last drawn segment of the line being drawn")}
-
-    def IsActive(self):
-        if FreeCADGui.ActiveDocument:
-            return True
-        else:
-            return False
 
 
 #---------------------------------------------------------------------------
@@ -923,9 +865,7 @@ FreeCADGui.addCommand('Draft_WireToBSpline',WireToBSpline())
 
 
 # context commands
-FreeCADGui.addCommand('Draft_FinishLine',FinishLine())
-FreeCADGui.addCommand('Draft_CloseLine',CloseLine())
-FreeCADGui.addCommand('Draft_UndoLine',UndoLine())
+
 FreeCADGui.addCommand('Draft_ToggleConstructionMode',ToggleConstructionMode())
 FreeCADGui.addCommand('Draft_ToggleContinueMode',ToggleContinueMode())
 FreeCADGui.addCommand('Draft_ApplyStyle',ApplyStyle())
