26eeec5a998c80f159fe4cfd8c8ccf68bd107347
vocx-fc <ecabreracs@gmail.com>
2019-08-17 23:22:09 -0500

    Draft: removed GuiCommand Draft_AddConstruction() from DraftTools.py

--- src/Mod/Draft/DraftTools.py
+++ src/Mod/Draft/DraftTools.py
@@ -589,35 +589,7 @@ class WireToBSpline(Modifier):
                             self.finish()
 
 
-class Draft_AddConstruction():
 
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_Construction',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_AddConstruction", "Add to Construction group"),
-                'ToolTip' : QtCore.QT_TRANSLATE_NOOP("Draft_AddConstruction", "Adds the selected objects to the Construction group")}
-
-    def Activated(self):
-        import FreeCADGui
-        if hasattr(FreeCADGui,"draftToolBar"):
-            col = FreeCADGui.draftToolBar.getDefaultColor("constr")
-            col = (float(col[0]),float(col[1]),float(col[2]),0.0)
-            gname = Draft.getParam("constructiongroupname","Construction")
-            grp = FreeCAD.ActiveDocument.getObject(gname)
-            if not grp:
-                grp = FreeCAD.ActiveDocument.addObject("App::DocumentObjectGroup",gname)
-            for obj in FreeCADGui.Selection.getSelection():
-                grp.addObject(obj)
-                obrep = obj.ViewObject
-                if "TextColor" in obrep.PropertiesList:
-                    obrep.TextColor = col
-                if "PointColor" in obrep.PropertiesList:
-                    obrep.PointColor = col
-                if "LineColor" in obrep.PropertiesList:
-                    obrep.LineColor = col
-                if "ShapeColor" in obrep.PropertiesList:
-                    obrep.ShapeColor = col
-                if hasattr(obrep,"Transparency"):
-                    obrep.Transparency = 80
 
 
 
@@ -694,7 +666,7 @@ FreeCADGui.addCommand('Draft_ToggleDisplayMode',ToggleDisplayMode())
 
 
 
-FreeCADGui.addCommand('Draft_AddConstruction',Draft_AddConstruction())
+
 
 # snap commands
 
