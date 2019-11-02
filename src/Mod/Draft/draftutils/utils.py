0c134518b4af261cb3f5550f8715ad848fa5ba9e
vocx-fc <vocx-fc@users.noreply.github.com>
2019-11-01 23:55:32 -0600

    New files for utilities

---
+++ src/Mod/Draft/draftutils/utils.py
@@ -0,0 +1,8 @@
+"""This module provides utility functions for the Draft Workbench
+"""
+## @package utils
+# \ingroup  DRAFT
+# \brief This module provides utility functions for the Draft Workbench
+
+arrowtypes = ["Dot", "Circle", "Arrow", "Tick", "Tick-2"]
+
